43de66574901a1c171e62c6de7859c4e7c351dc5
Slobodan <sloooba@yahoo.com>
2022-10-20 16:07:14 +0200

    Space removed

--- booster-sweeper.php
+++ booster-sweeper.php
@@ -1,22 +1,22 @@
 <?php
-    /**
-     * Plugin Name:         Booster Sweeper
-     * Description:         Boost the speed by sweeping assets your pages do not need
-     *
-     * PHP version  7.3.5
-     *
-     * @category Optimization
-     * @package  Booster_Sweeper
-     * @author   MaxPressy <webmaster@maxpressy.com>
-     * @license  GPL v2 or later
-     * @link     maxpressy.com
-     *
-     * Author URI:          https://maxpressy.com
-     * Version:             0.0.2
-     * Text Domain:         booster_sweeper
-     * Domain Path:         /languages
-     * Requires at least:   5.8
-     */
+/**
+ * Plugin Name:         Booster Sweeper
+ * Description:         Boost the speed by sweeping assets your pages do not need
+ *
+ * PHP version  7.3.5
+ *
+ * @category Optimization
+ * @package  Booster_Sweeper
+ * @author   MaxPressy <webmaster@maxpressy.com>
+ * @license  GPL v2 or later
+ * @link     maxpressy.com
+ *
+ * Author URI:          https://maxpressy.com
+ * Version:             0.0.2
+ * Text Domain:         booster_sweeper
+ * Domain Path:         /languages
+ * Requires at least:   5.8
+ */
 
 if (! defined('ABSPATH')) { 
     exit; 
