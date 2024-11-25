b2a85046e4d077e7debc561c5f3bed145f1a3909
Slobodan <sloooba@yahoo.com>
2024-11-25 19:48:20 +0100

    Set parameters on the get_plugin_data

--- booster-sweeper.php
+++ booster-sweeper.php
@@ -27,7 +27,7 @@ if (! defined('ABSPATH')) {
 if (! function_exists('get_plugin_data')) {
     include_once ABSPATH .'wp-admin/includes/plugin.php';
 }
-$plugin_data = get_plugin_data(__FILE__);
+$plugin_data = get_plugin_data(__FILE__, true, false);
 define('BOOSTER_SWEEPER', ($plugin_data && $plugin_data['Name']) ? $plugin_data['Name'] : 'Booster Sweeper');
 define('BOOSTER_SWEEPER_VERSION', ($plugin_data && $plugin_data['Version']) ? $plugin_data['Version'] : '1.0.0');
 
